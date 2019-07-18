5e4855934c2d85b0ce1b7d16f79b0cc1fb781c10
Stenfrank <faguirre@soenac.com>
2019-07-17 22:31:13 -0500

    Fixed error in loading CSV files for Windows

--- database/seeds/ConfigurationSeeder.php
+++ database/seeds/ConfigurationSeeder.php
@@ -5,85 +5,86 @@ use Illuminate\Database\Seeder;
 class ConfigurationSeeder extends Seeder
 {
     /**
-     * Prefix
+     * Prefix.
+     *
      * @var string
      */
     public $prefix = 'csv';
 
     /**
-     * Tables
+     * Tables.
+     *
      * @var array
      */
     public $tables = [
         'type_organizations' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'countries' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'departments' => [
-            'columns' => 'id, country_id, name, code, @created_at, @updated_at'
+            'columns' => 'id, country_id, name, code, @created_at, @updated_at',
         ],
         'municipalities' => [
-            'columns' => 'id, department_id, name, code, @created_at, @updated_at'
+            'columns' => 'id, department_id, name, code, @created_at, @updated_at',
         ],
         'type_document_identifications' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'taxes' => [
-            'columns' => 'id, name, description, code, @created_at, @updated_at'
+            'columns' => 'id, name, description, code, @created_at, @updated_at',
         ],
         'type_regimes' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'type_liabilities' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'payment_forms' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'payment_methods' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'discounts' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'type_currencies' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'unit_measures' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'reference_prices' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'type_documents' => [
-            'columns' => 'id, name, code, cufe_algorithm, prefix, @created_at, @updated_at'
+            'columns' => 'id, name, code, cufe_algorithm, prefix, @created_at, @updated_at',
         ],
         'type_item_identifications' => [
-            'columns' => 'id, name, code, code_agency, @created_at, @updated_at'
+            'columns' => 'id, name, code, code_agency, @created_at, @updated_at',
         ],
         'type_operations' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'type_environments' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
         'languages' => [
-            'columns' => 'id, name, code, @created_at, @updated_at'
+            'columns' => 'id, name, code, @created_at, @updated_at',
         ],
     ];
 
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run() {
+    public function run()
+    {
         foreach ($this->tables as $key => $table) {
             DB::connection()
                 ->getpdo()
-                ->exec("LOAD DATA LOCAL INFILE '".public_path($this->prefix.DIRECTORY_SEPARATOR."{$key}.{$this->prefix}")."' INTO TABLE $key({$table['columns']}) SET created_at = NOW(), updated_at = NOW()");
+                ->exec("LOAD DATA LOCAL INFILE '".str_replace(DIRECTORY_SEPARATOR, '/', public_path($this->prefix.DIRECTORY_SEPARATOR."{$key}.{$this->prefix}"))."' INTO TABLE $key({$table['columns']}) SET created_at = NOW(), updated_at = NOW()");
         }
     }
 }
